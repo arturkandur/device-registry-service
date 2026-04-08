6514a6e63ad5539c88a24cc3ac6461ca38f90f64
arturkandur <arturkandur@gmail.com>
2026-04-08 12:25:51 +0300

    feat: Init Spring Boot

---
+++ src/main/java/com/akv/service/DeviceRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.akv.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceRegistryServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DeviceRegistryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akv/service/DeviceRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akv.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceRegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
